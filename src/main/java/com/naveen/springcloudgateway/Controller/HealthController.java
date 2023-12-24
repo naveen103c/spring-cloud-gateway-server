5e484849319347ca68f1a9a6b3a3b1f9f218395e
naveen103c <naveenchaudhary103@gmail.com>
2023-12-24 13:20:33 +0530

    Spring Cloud Gateway without naming server

---
+++ src/main/java/com/naveen/springcloudgateway/Controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.naveen.springcloudgateway.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String status(){
+        return "up";
+    }
+}
